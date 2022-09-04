27dee47816f449b54d055f2de73f4f212661d367
Joshua <joshuayuanhu@gmail.com>
2022-09-04 16:17:08 +0200

    updates

--- bs4_first.py
+++ bs4_first.py
@@ -4,21 +4,19 @@ import requests
 url = "https://yuan-hu.blogspot.com/2022/07/joshuas-first-post.html"
 page = requests.get(url)
 soup = bs4.BeautifulSoup(page.content, "html.parser")  # why should there be no parentheses after "content"?
-# Content is a data item not a function, so by saying page.content, you're accessing
+# Content is a data item not a function, so by saying page.content, you're accessing a data item of an instance of a
+# class (e.g. self.name)
 
 
 def print_links():
-    links = soup.find_all("a")  # The a stands for "anchor"
-    for link in links:
-        link = link.get("href")
-        if link:
-            print(link)
-
     # This function prints None a couple of times in between the links. Is it because there are links that have the
-    # "a" tag but don't have href attributes? For example, "Yu'an Hu" contains a link but the link isn't displayed
-    """links = [link.get('href') for link in soup.find_all("a")]  # the "a" is a tag that stands for hyperlinks
+    # "a" tag but don't have href attributes? Yes. Although this is valid html, it is not a good idea
+    # Anchor tags ("a" tags) should contain hyperlinks, which come in the form of href attributes, so if there is no
+    # href attribute, there is no link, meaning there is no real point in having an anchor tag there
+    links = [link.get('href') for link in soup.find_all("a") if link.get("href") is not None]
+    # the "a" is a tag that stands for hyperlinks
     for link in links:
-        print(link)"""
+        print(link)
 
 
 if __name__ == "__main__":
